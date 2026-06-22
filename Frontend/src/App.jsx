3f5252a9c4c8713c51f50785e327e4be2d8f164b
jackboy04 <cheikhdjibrilsow949@gmail.com>
2026-06-22 10:11:40 +0000

    feat: pages AccueilBackOffice et Rapports avec navigation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,22 +21,19 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* ── 2. DÉFINITION DE LA PAGE PAR DÉFAUT ── */}
-        <Route path="/" element={<Navigate to="/validation" replace />} />
-        {/* ── 3. DÉCLARATION DES ROUTES DE NAVIGATION ── */}
-        /* Route pour l'Espace Chauffeur */
+        <Route path="/" element={<Navigate to="/Accueilbackoff" replace />} />
+
         <Route path="/Accueilchauffeur" element={<HomeChauffeur />} />
         <Route path="/Declaration" element={<Declaration />} />
         <Route path="/SuiviChauffeur" element={<SuiviChauffeur />} />
-        /* Route Backoffice */
+
         <Route path="/validation" element={<ValidationListe />} />
         <Route path="/HistoBackoff" element={<HistoriqueBackoffice />} />
-        <Route path="/Declaration" element={<Declaration />} />
         <Route path="/Accueilbackoff" element={<AccueilBackOffice />} />
         <Route path="/Rapport" element={<Rapports />} />
-        /*Route page de connexion */
+
         <Route path="/login" element={<Login />} />
-        /*Route mecano */
+
         <Route path="/Accueilmeca" element={<HomeMecano />} />
         <Route path="/Affectationsmeca" element={<AffectationsMecano />} />
         <Route path="/Etatreparation" element={<SuiviReparation />} />
